d2fbffa438f86f0d5c10b900878dbe57d5ca40b5
eunhwa99 <ekdusp89@gmail.com>
2025-08-31 17:59:55 +0900

    test

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Helm values.yaml에서 주입한 URL로 JS 치환
-sed -i "s|REACT_APP_BACKEND_URL_PLACEHOLDER|$REACT_APP_BACKEND_URL|g" /app/build/static/js/*.js
+
+echo "Replacing placeholder with $REACT_APP_BACKEND_URL"
+# 빌드된 JS나 index.html 파일을 정확히 치환
+sed -i "s|REACT_APP_BACKEND_URL_PLACEHOLDER|$REACT_APP_BACKEND_URL|g" /app/build/index.html
 
 # React 앱 실행
 serve -s build -l 3000
